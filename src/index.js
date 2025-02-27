fe40f97c032ea7e343f0057e843a64fb61d07f05
amardeep.singh <amardeep.singh@salesforce.com>
2025-02-27 14:03:31 -0800

    Connecting local repo to GitHub

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css';
+import './assets/css/responsive.css';
+import './assets/css/multistep.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
