5bfbed2bddc2d019ae34e013d593f7fbcda33fcc
amardeep.singh <amardeep.singh@salesforce.com>
2025-03-07 00:13:19 -0800

    added waitlist

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import TopBar from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Services2 from './components/Services2';
-import Careers from './components/Careers';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Services2 from "./components/Services2";
+import Careers from "./components/Careers";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Waitlist from "./components/waitlist";
 
 function App() {
   return (
-    <div className="light-mode">
-      <div className="main_wrapper">
-        <TopBar />       
-        <div className="main_content">
-          <Hero />
-          <About />
-          <Services />
-          <Services2 />
-          <Careers />
-          <Contact />
+    <Router>
+      <div className="light-mode">
+        <div className="main_wrapper">
+          <Routes>
+            {/* Home Page - Shows Everything */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <div className="main_content">
+                    <Hero />
+                    <About />
+                    <Services />
+                    <Services2 />
+                    <Careers />
+                    <Contact />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+
+            {/* Waitlist Page - Shows ONLY Header and Waitlist */}
+            <Route
+              path="/waitlist"
+              element={
+                <>
+                  <Header />
+                  <Waitlist />
+                </>
+              }
+            />
+          </Routes>
         </div>
-        <Footer />
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    baseUrl:'https://api.ohanabehavioralservice.com'  
+   // baseUrl:'http://localhost:8080' 
+  };
+  
+  export default config;
