2b1f9cfbe70fc72681ed7526f5a6627a034ad97e
amardeep.singh <amardeep.singh@salesforce.com>
2025-11-29 18:52:41 -0800

    fixed career section

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ import '../src/assets/css/hero-polish.css';
 import '../src/assets/css/insurance.css';
 import '../src/assets/css/modern-footer.css';
 import '../src/assets/css/better-spacing.css';
-import '../src/assets/css/optimized-image.css';
-import '../src/assets/css/contact-form.css';
+import '../src/assets/css/careers-form.css';
 
 function AppContent() {
   // Track page views on route changes
